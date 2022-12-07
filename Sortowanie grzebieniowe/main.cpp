1daecb3c3f6accdb9dc27ebf0e1a26ad0eac3243
DamSte09 <damste09@gmail.com>
2022-12-07 19:37:34 +0100

    wczytywanie i zapisywanie do pliku, zaczecie sprawozdania

--- Sortowanie grzebieniowe/main.cpp
+++ Sortowanie grzebieniowe/main.cpp
@@ -3,21 +3,26 @@
 #include <cstdlib>
 #include <time.h>
 #include <iomanip>
+#include <fstream>
 
 using namespace std;
 
 //Tworzenie zioru i wypelnianie go liczbami pseudolosowymi 0-10
-void randArray(int tab[], int n) {
+void inputArray(int tab[], int size) {
+	fstream inFile;
+	inFile.open("tablica.txt");
 	cout<<"\nPrzed sortowaniem:\n\n";
-	srand((unsigned)time(NULL));
-	for(int i=0; i<n; i++) {
-		tab[i]=rand()%10;
-		cout << setw(2) << tab[i];
+	//srand((unsigned)time(NULL));
+	for(int i=0; i<size; i++) {
+		//tab[i]=rand()%1000;
+		inFile>>tab[i];
+		cout << setw(5) << tab[i];
 	}
 	cout<<endl;
+	inFile.close();
 }
 
-// Znalezienie odstepu miedzy elementami
+// Funkcja znajdujaca odstep miedzy elementami
 int getNextGap(int gap) {
     // Zmiejszanie sie odstepu przez wspolczynnik
     gap=(gap*10)/13;
@@ -29,31 +34,38 @@ int getNextGap(int gap) {
 // Sortowanie grzebieniowe
 void combSort(int* tab, int size) {
    int gap=size, tmp;
-   bool swapped=true;
-   while (gap!=1 || swapped) { // jesli gap=1 lub nie dokonano zamiany - wyjscie z petli     
+   bool swapped=true;	//zainicjowanie zmiennej prawda/falsz
+   while (gap!=1 || swapped) {      // jesli gap=1 lub nie dokonano zamiany - wyjscie z petli
       gap=getNextGap(gap);
       swapped=false;
-      for ( int i=0; i+gap<size; i++ ) { // wykonuje od 0 do ostatniego elementu tablicy 
-        if ( tab[i + gap] < tab[i] ) {   // porownanie elementow odleglych o rozpietosc
-            swap(tab[i], tab[i+gap]); // zamiana elementow
+      // wykonuje od 0 do ostatniego elementu tablicy
+      for (int i=0; i+gap<size; i++) {  
+        if (tab[i + gap] < tab[i]) { // porownanie elementow odleglych o rozpietosc
+            swap(tab[i], tab[i+gap]); // zamiana elementow 
             swapped = true;
         }
       }
    }
 }
 
-
 int main()
 {
+	clock_t start, stop;
+	double czas;
 	int n;
 	cout<<"Podaj dlugosc tablicy: ";
 	cin>>n;
 	int tab[n];
-	randArray(tab, n);
+	inputArray(tab, n);
 	cout<<endl<<endl;
+	start=clock();
+	//Wywolanie funkcji
 	combSort(tab, n);
+	stop=clock();
 	cout << endl << "Po sortowaniu:\n\n";
-	for(int i = 0; i < n; i++) cout << setw(2) << tab[i];	
+	czas = (double)(stop - start)/CLOCKS_PER_SEC;
+	cout<<endl<< "czas: "<< czas;
+	for(int i = 0; i < n; i++) cout << setw(5) << tab[i];	
 	cout << endl << endl;
 	return 0;
 }
--- Sortowanie kopcowe/main.cpp
+++ Sortowanie kopcowe/main.cpp
@@ -3,59 +3,55 @@
 #include <cstdlib>
 #include <time.h>
 #include <iomanip>
-
+#include <fstream>
 using namespace std;
 
 
-//Tworzenie tablicy i wypelnianie jej liczbami pseudolosowymi od 0 do 10
-// Oraz wypisywanie jej
-void randArray(int arr[], int N)
-{
-	cout<<"Przed sortowaniem:\n";
-	srand((unsigned)time(NULL));
-	for(int i=0; i<N; i++)
-	{
-		arr[i]=rand()%10;
+//Tworzenie tablicy oraz wypisywanie jej
+void inputArray(int arr[], long int size) {
+	//cout<<"Przed sortowaniem:\n";
+	fstream inFile;
+	inFile.open("tablica.txt");
+	
+	//srand((unsigned)time(NULL));
+	for(int i=0; i<size; i++) {
+		//arr[i]=rand()%1000;	//wypelnianie jej liczbami pseudolosowymi od 0 do 1000
+		inFile>>arr[i];
 		cout << setw(2) << arr[i];
 	}
 	cout<<endl;
+	inFile.close();
 }
 
 // Ulozenie poddrzewa zakorzenionego w wezle i
 // ktory jest indeksem w arr[].
 // N jest rozmiarem tablicy
-void heapify(int arr[], int N, int i)
-{
-    // Zainicjowanie largest jako korzenia
-    int largest=i;
-    // lewy potomek = 2*i + 1
-    int left=2*i+1;
-    // prawy potomek = 2*i + 2
-    int right=2*i+2;
+void heapify(int arr[], int size, int i) {
+    int largest=i;	// Zainicjowanie largest jako korzenia
+    int left=2*i+1;	// lewy potomek = 2*i + 1
+    int right=2*i+2;// prawy potomek = 2*i + 2
     // Jesli lewy potomek jest wiekszy niz korzen
-    if (left<N && arr[left]>arr[largest])
+    if (left<size && arr[left]>arr[largest])
         largest=left;
     // Jesli prawy potomek jest wiekszy niz korzen
-    // do tej pory
-    if (right<N && arr[right]>arr[largest])
+    if (right<size && arr[right]>arr[largest])
         largest=right;
     // Jesli najwiekszy nie jest korzeniem
     if (largest!=i)
 	{
         swap(arr[i], arr[largest]);
-        // Rekursywne kopcowanie dotknietego poddrzewa
-        heapify(arr, N, largest);
-    }
+        // Rekursywne kopcowanie
+        heapify(arr, size, largest);
+    }    
 }
  
 // Glowna funkcja wykonujaca sortowanie kopcowe
-void heapSort(int arr[], int N)
-{
+void heapSort(int arr[], int size) {
     // Budowanie kopca
-    for (int i=N/2-1; i>=0; i--)
-        heapify(arr, N, i);
+    for (int i=size/2-1; i>=0; i--)
+        heapify(arr, size, i);
     // Wyodrebnianie elementu jeden po drugim z kopca
-    for (int i=N-1; i>0; i--) 
+    for (int i=size-1; i>0; i--) 
 	{
         // Przeniesienie korzenia na koniec
         swap(arr[0], arr[i]);
@@ -65,26 +61,36 @@ void heapSort(int arr[], int N)
 }
  
 // Wypisywanie posortowanej tablicy n wymiarowej
-void printArray(int arr[], int N)
-{
-    for (int i=0; i<N; i++)
-        cout << setw(2) << arr[i];
+void printArray(int arr[], int size) {
+	ofstream wyjscie;
+	wyjscie.open("wyjscie.txt");
+    for (int i=0; i<size; i++)
+    {
+        cout << setw(5) << arr[i];
+        //wyjscie<< arr[i]<<" ";
+    }
     cout << endl;
+    wyjscie.close();
 }
 
 
-int main()
-{
+int main() {
+	clock_t start, stop;
+	double czas;
 	int n;
-	cout<<"Podaj rozmiar tablicy ktora bedzie wylosowana: ";
+	cout<<"Podaj rozmiar losowanej: ";
 	cin>>n;
     int arr[n];
     // Losowanie tablicy
- 	randArray(arr, n);
+ 	inputArray(arr, n);
+ 	start=clock();
     // Wywolanie funkcji heapSort
     heapSort(arr, n);
+    stop=clock();
+    czas = (double)(stop - start)/CLOCKS_PER_SEC;
     cout << "Po sortowaniu\n";
     //Wypisanie posortowanej tablicy
     printArray(arr, n);
+    //printf("%.30f", czas);
     return 0;
 }
